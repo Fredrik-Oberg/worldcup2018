654ee44a9e78e680abe4e789da0c6f45c43f8018
Fredrik Öberg <fredrik.oberg@ezy.se>
2018-06-12 23:27:42 +0200

    Commit moved files changes

---
+++ src/betscraping/RedisLayer/Redis.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using ServiceStack.Redis;
+using static System.Environment;
+
+namespace RedisLayer
+{
+
+    public class Redis
+    {
+        private int database { get; set; }
+        private string redisConnectionString { get; }
+
+        public Redis(int db)
+        {
+            database = db;
+            redisConnectionString = GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT").Equals("release", StringComparison.OrdinalIgnoreCase)
+                ? "db:6379"
+                : "127.0.0.1:6379";
+        }
+
+        public void SetRedisValue<T>(T value, string key)
+        {
+            var clientsManager = new BasicRedisClientManager(redisConnectionString);
+            using (IRedisClient redis = clientsManager.GetClient())
+            {
+                redis.Db = database;
+                redis.Set<T>(key, value);
+            }
+        }
+
+        public T GetRedisValue<T>(string key)
+        {
+            var clientsManager = new BasicRedisClientManager(redisConnectionString);
+            using (IRedisClient redis = clientsManager.GetClient())
+            {
+                redis.Db = database;
+                return redis.Get<T>(key);
+            }
+        }
+        public List<string> GetAllKeys()
+        {
+            var clientsManager = new BasicRedisClientManager(redisConnectionString);
+            using (IRedisClient redis = clientsManager.GetClient())
+            {
+                redis.Db = database;
+                return redis.GetAllKeys();
+
+            }
+        }
+
+    }
+}
