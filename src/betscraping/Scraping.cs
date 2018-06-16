37e487e288f5374e14ad5fd3bf9d8840ced0dede
Fredrik Öberg <fredrik.oberg@ezy.se>
2018-06-16 15:55:21 +0200

    - Add correct number of participants - use same driver for each scraping, so we do not dispose and create a new session - Add retry for scraping of participant - Better logging

--- src/betscraping/Scraping.cs
+++ src/betscraping/Scraping.cs
@@ -4,6 +4,7 @@ using System.Collections.ObjectModel;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using betscraping.Models;
 using Newtonsoft.Json;
@@ -19,7 +20,7 @@ namespace betscraping
     public class Scraping
     {
         private const string Participants = "participants:";
-        private const string Isbusy = "IsBusy";
+        private const string IsBusy = "IsBusy";
         private const string Lastupdatematch = "LastUpdateMatch";
 
         private int NumberOfParticipants { get; }
@@ -28,16 +29,16 @@ namespace betscraping
 
         public Scraping()
         {
-            NumberOfParticipants = 33;
+            NumberOfParticipants = 34;
             Database = 10;
             GameId = "55Pi";
         }
-        
-        private void WriteEvent(string msg)
+
+        private static void WriteEvent(string msg)
         {
             try
             {
-                Console.WriteLine(msg);
+                Console.WriteLine("- " + msg);
             }
             catch (Exception e)
             {
@@ -59,28 +60,33 @@ namespace betscraping
 
             var savedLastMatchUpdate = redis.GetRedisValue<DateTime>(Lastupdatematch);
 
-            var isBusy = redis.GetRedisValue<bool>(Isbusy);
+            var isBusy = redis.GetRedisValue<bool>(IsBusy);
 
             if (scheduledMatchUtc < DateTime.UtcNow && savedLastMatchUpdate != scheduledMatchUtc && isBusy == false)
             {
-                redis.SetRedisValue(true, Isbusy);
                 WriteEvent("Start scraping");
-               
+
+                var driver1 = NewWebDriver();
+                var driver2 = NewWebDriver();
+                var driver3 = NewWebDriver();
+
                 try
                 {
+                    redis.SetRedisValue(true, IsBusy);
+
                     for (var i = 0; i < NumberOfParticipants; i += 3)
                     {
                         var tasks = new[]
                         {
-                            
+
                             Task.Factory.StartNew(() =>
-                                Scrape(NewWebDriver(),
+                                TryScrape(driver1,
                                     i, schedulesConcurrentMatches, scheduledMatchUtc, redis)),
                             Task.Factory.StartNew(() =>
-                                Scrape(NewWebDriver(),
+                                TryScrape(driver2,
                                     i + 1, schedulesConcurrentMatches, scheduledMatchUtc, redis)),
                             Task.Factory.StartNew(() =>
-                                Scrape(NewWebDriver(),
+                                TryScrape(driver3,
                                     i + 2, schedulesConcurrentMatches, scheduledMatchUtc, redis))
                         };
 
@@ -90,26 +96,158 @@ namespace betscraping
                     redis.SetRedisValue(scheduledMatchUtc, Lastupdatematch);
                 }
 
-
                 catch (Exception e)
                 {
-                    Console.WriteLine(e.Message);
-                    Console.WriteLine(e.StackTrace);
+                    WriteEvent($"Scraping:{NewLine}{e.Message}{NewLine}{e.StackTrace}");
                 }
                 finally
                 {
-                    redis.SetRedisValue(false, Isbusy);
+                    driver1.Quit();
+                    driver2.Quit();
+                    driver3.Quit();
+
+                    redis.SetRedisValue(false, IsBusy);
                 }
             }
             else
             {
-                WriteEvent($"No scraping needed currentTime: {DateTime.UtcNow} {Environment.NewLine}" +
-                           $"|{nameof(scheduledMatchUtc)}:{scheduledMatchUtc} {Environment.NewLine}" +
-                           $"|{nameof(savedLastMatchUpdate)}:{savedLastMatchUpdate} {Environment.NewLine}" +
-                           $"|{nameof(isBusy)}:{isBusy} {Environment.NewLine}");
+                WriteEvent($"No scraping needed currentTime: {DateTime.UtcNow} {NewLine}" +
+                           $"|{nameof(scheduledMatchUtc)}:{scheduledMatchUtc} {NewLine}" +
+                           $"|{nameof(savedLastMatchUpdate)}:{savedLastMatchUpdate} {NewLine}" +
+                           $"|{nameof(isBusy)}:{isBusy} {NewLine}");
             }
         }
 
+        private void TryScrape(IWebDriver driver,
+            int index,
+            int schedulesConcurrentMatches,
+            DateTime scheduledMatchUtc,
+            Redis redis)
+        {
+            if (index > NumberOfParticipants - 1) return;
+
+            TryAgainException(() => Scrape(driver, index, schedulesConcurrentMatches, scheduledMatchUtc, redis), 10);
+        }
+
+        private bool Scrape(IWebDriver driver,
+            int index,
+            int schedulesConcurrentMatches,
+            DateTime scheduledMatchUtc,
+            Redis redis)
+        {
+            var concurrentMatchesScraped = 0;
+
+            driver.Navigate().GoToUrl($"http://www.betme.se/game.html?id={GameId}");
+            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
+
+            var usersTabSelect = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("usersTabSelect")));
+            var userForm = TryAgainException(() =>
+            {
+                usersTabSelect.Click();
+                return wait.Until(ExpectedConditions.ElementIsVisible(By.Id("usersForm")));
+            });
+            var participantsRow = userForm.FindElements(By.CssSelector("tr"));
+
+            //Get participants row for current index
+            var partRow = participantsRow[index];
+
+            var participant = partRow.FindElement(By.CssSelector("a"));
+
+            var participantScore = partRow.FindElements(By.CssSelector("td"))[2];
+
+            //Get the score
+            var points = participantScore.Text.Replace("Points", "");
+            var participantText = $"{participant.Text} {points}";
+
+            WriteEvent(participantText);
+
+            var user = redis.GetRedisValue<Participant>(Participants + participant.Text)
+                       ?? new Participant(participant.Text);
+
+            user.Points = points;
+
+            if (participant.Displayed)
+            {
+                participant.Click();
+            }
+            else
+            {
+                ((IJavaScriptExecutor)driver).ExecuteScript("arguments[0].click();", participant);
+            }
+
+            ReadOnlyCollection<IWebElement> boxes = null;
+            boxes = TryAgainException(() =>
+            {
+                IWebElement userTabToRender = null;
+                userTabToRender = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("userTabToRender")));
+                return userTabToRender.FindElements(By.ClassName("boxHalfInTab"));
+            });
+
+            foreach (var box in boxes)
+            {
+                if (concurrentMatchesScraped == schedulesConcurrentMatches)
+                {
+                    break;
+                }
+
+                IWebElement table = null;
+                table = TryAgainException(() => box.FindElement(By.ClassName("eventRow")));
+
+                ReadOnlyCollection<IWebElement> rows = null;
+                //Get all rows except for time row, that is in .rowsepartor
+                rows = TryAgainException(() => table.FindElements(By.CssSelector("tr:not(.rowSeparator)")));
+
+                var rowIndex = 0;
+                ReadOnlyCollection<IWebElement> rowSeparators = null;
+
+                //in this container the match start timer is found
+                rowSeparators = TryAgainException(() => table.FindElements(By.CssSelector("tr.rowSeparator")));
+
+                foreach (var row in rows)
+                {
+                    var match = new Match();
+
+                    var matchTimeutc = GetMatchTimeutc(rowSeparators, rowIndex++);
+
+                    if (scheduledMatchUtc != matchTimeutc)
+                    {
+                        continue;
+                    }
+
+                    WriteEvent($"{user.Name}: {nameof(matchTimeutc)}: {matchTimeutc}");
+
+                    match.MatchStart = matchTimeutc;
+                    match.HomeTeam = GetTextFromFindElementByClass(row, "eventRowLeft");
+                    match.AwayTeam = GetTextFromFindElementByClass(row, "eventRowRight");
+
+                    WriteEvent($"{user.Name}: {nameof(match.HomeTeam)}:{match.HomeTeam} - {nameof(match.AwayTeam)}:{match.AwayTeam}");
+
+                    var events = TryAgainException(() => row.FindElement(By.ClassName("eventRowCenter")));
+                    var fields = TryAgainException(() => events.FindElements(By.CssSelector("input")));
+
+                    var result = fields[0].GetAttribute("value") + " - " + fields[1].GetAttribute("value");
+
+                    match.Result = result;
+                    WriteEvent($"{user.Name}: {nameof(match.Result)}: {match.Result}");
+
+                    RemoveOldMatch(user, match);
+
+                    user.Matches.Add(match);
+
+                    concurrentMatchesScraped++;
+
+                    if (concurrentMatchesScraped == schedulesConcurrentMatches)
+                    {
+                        break;
+                    }
+                }
+            }
+
+            user.LastUpdated = DateTime.Now.ToString("O");
+
+            redis.SetRedisValue(user, Participants + user.Name);
+            return true;
+        }
         private static RemoteWebDriver NewWebDriver()
         {
             var driverAddress = GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT").Equals("release", StringComparison.OrdinalIgnoreCase)
@@ -123,7 +261,7 @@ namespace betscraping
         {
             using (StreamReader r = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "/assets/schedule.json"))
             {
-                string json = r.ReadToEnd();
+                var json = r.ReadToEnd();
                 var schedules = JsonConvert.DeserializeObject<IList<Schedule>>(json);
                 foreach (var schedule in schedules)
                 {
@@ -133,9 +271,10 @@ namespace betscraping
                     var scheduledMatchUtc = DateTime.SpecifyKind(parsedGameTime, DateTimeKind.Utc);
 
                     var hoursToAdd = 1.5;
-                    Console.WriteLine($"scheduledMatchUtc + {hoursToAdd}:{scheduledMatchUtc.AddHours(hoursToAdd)}{Environment.NewLine} UTCTimeNow:{DateTime.UtcNow}");
-                    
+
                     if (scheduledMatchUtc.AddHours(hoursToAdd) < DateTime.UtcNow) continue;
+
+                    WriteEvent($"scheduledMatchUtc + {hoursToAdd}:{scheduledMatchUtc.AddHours(hoursToAdd)}{NewLine} UTCTimeNow:{DateTime.UtcNow}");
 
                     schedulesConcurrentMatches = schedule.ConcurrentGames;
                     return scheduledMatchUtc;
@@ -144,144 +283,6 @@ namespace betscraping
 
             return DateTime.MaxValue;
         }
-
-        private void Scrape(IWebDriver driver,
-            int index,
-            int schedulesConcurrentMatches,
-            DateTime scheduledMatchUtc,
-            Redis redis)
-        {
-            var concurrentMatchesScraped = 0;
-            try
-            {
-                if(index > NumberOfParticipants - 1) return;
-                
-                driver.Navigate().GoToUrl($"http://www.betme.se/game.html?id={GameId}");
-                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(30));
-
-                var usersTabSelect = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("usersTabSelect")));
-                var userForm = TryAgainException(() =>
-                {
-                    usersTabSelect.Click();
-                    return wait.Until(ExpectedConditions.ElementIsVisible(By.Id("usersForm")));
-                });
-                var participantsRow = userForm.FindElements(By.CssSelector("tr"));
-
-                //Get participants row for current index
-                var partRow = participantsRow[index];
-
-                var participant = partRow.FindElement(By.CssSelector("a"));
-
-                var participantScore = partRow.FindElements(By.CssSelector("td"))[2];
-
-                //Get the score
-                var points = participantScore.Text.Replace("Points", "");
-                var participantText = $"{participant.Text} {points}";
-
-                WriteEvent(participantText);
-
-                var user = redis.GetRedisValue<Participant>(Participants + participant.Text)
-                           ?? new Participant(participant.Text);
-
-                user.Points = points;
-
-                if (participant.Displayed)
-                {
-                    participant.Click();
-                }
-                else
-                {
-                    ((IJavaScriptExecutor) driver).ExecuteScript("arguments[0].click();", participant);
-                }
-
-                ReadOnlyCollection<IWebElement> boxes = null;
-                boxes = TryAgainException(() =>
-                {
-                    IWebElement userTabToRender = null;
-                    userTabToRender = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("userTabToRender")));
-                    return userTabToRender.FindElements(By.ClassName("boxHalfInTab"));
-                });
-
-                foreach (var box in boxes)
-                {
-                    if (concurrentMatchesScraped == schedulesConcurrentMatches)
-                    {
-                        break;
-                    }
-
-                    IWebElement table = null;
-                    table = TryAgainException(() => box.FindElement(By.ClassName("eventRow")));
-
-                    ReadOnlyCollection<IWebElement> rows = null;
-                    //Get all rows except for time row, that is in .rowsepartor
-                    rows = TryAgainException(() => table.FindElements(By.CssSelector("tr:not(.rowSeparator)")));
-
-                    var rowIndex = 0;
-                    ReadOnlyCollection<IWebElement> rowSeparators = null;
-
-                    //in this container the match start timer is found
-                    rowSeparators = TryAgainException(() => table.FindElements(By.CssSelector("tr.rowSeparator")));
-
-                    foreach (var row in rows)
-                    {
-                        var match = new Match();
-
-                        var matchTimeutc = GetMatchTimeutc(rowSeparators, rowIndex++);
-
-                        WriteEvent($"{nameof(matchTimeutc)}:{matchTimeutc}");
-
-                        if (scheduledMatchUtc != matchTimeutc)
-                        {
-                            continue;
-                        }
-
-                        match.MatchStart = matchTimeutc;
-                        match.HomeTeam = GetTextFromFindElementByClass(row, "eventRowLeft");
-                        match.AwayTeam = GetTextFromFindElementByClass(row, "eventRowRight");
-
-                        WriteEvent($"{nameof(match.HomeTeam)}:{match.HomeTeam}" +
-                                   " - " +
-                                   $"|{nameof(match.AwayTeam)}:{match.AwayTeam}");
-
-                        var events = TryAgainException(() => row.FindElement(By.ClassName("eventRowCenter")));
-                        var fields = TryAgainException(() => events.FindElements(By.CssSelector("input")));
-
-                        var result = fields[0].GetAttribute("value") + " - " + fields[1].GetAttribute("value");
-
-                        match.Result = result;
-                        WriteEvent($"{nameof(match.Result)}:{match.Result}");
-
-                        RemoveOldMatch(user, match);
-
-                        user.Matches.Add(match);
-
-                        concurrentMatchesScraped++;
-
-                        if (concurrentMatchesScraped == schedulesConcurrentMatches)
-                        {
-                            break;
-                        }
-                    }
-
-                    WriteEvent("-------------------------------------------------");
-                }
-
-                user.LastUpdated = DateTime.Now.ToString("O");
-
-                redis.SetRedisValue(user, Participants + user.Name);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-
-                Console.WriteLine(e.StackTrace);
-            }
-            finally
-            {
-                driver.Quit();
-            }
-        }
-
         private static void RemoveOldMatch(Participant user,
             Match match)
         {
@@ -316,14 +317,19 @@ namespace betscraping
             return TimeZoneInfo.ConvertTimeToUtc(unset, tz);
         }
 
-        public static T TryAgainException<T>(Func<T> action)
+        private static T TryAgainException<T>(Func<T> action)
         {
             return TryAgain(action, 0, 3);
+        }
+        private static T TryAgainException<T>(Func<T> action, int sleep)
+        {
+            return TryAgain(action, 0, 3, sleep);
         }
 
         private static T TryAgain<T>(Func<T> action,
             int i,
-            int limit)
+            int limit,
+            int sleep = 0)
         {
             try
             {
@@ -331,12 +337,18 @@ namespace betscraping
             }
             catch (Exception e)
             {
+                WriteEvent($"TryAgainException: i:{i} {NewLine}{e.Message}{NewLine}{e.StackTrace}");
+
                 if (i >= limit)
                 {
                     throw e;
                 }
 
                 var j = i + 1;
+                if (sleep > 0)
+                {
+                    Thread.Sleep(new TimeSpan(0, 0, 0, sleep));
+                }
                 return TryAgain(action, j, limit);
             }
         }
